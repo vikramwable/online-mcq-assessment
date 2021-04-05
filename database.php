5e369f880a2febe1d26a5a15e7755d4276526f8a
Vikram Wable <vikram.wable@healthfidelity.com>
2021-04-05 14:20:07 +0530

    adding creadentials for db

--- database.php
+++ database.php
@@ -1,3 +1,3 @@
 <?php
-$con=mysqli_connect("127.0.0.1","root","root","quiz_new") or die('Database not connected');
+$con=mysqli_connect("127.0.0.1","root","WPnjtHQJmTE4","quiz_new") or die('Database not connected');
 ?>
